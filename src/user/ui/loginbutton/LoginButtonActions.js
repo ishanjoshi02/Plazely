948e5cb29d3e5748c7b1da2cbc2dbe8069a17920
Ishan Joshi <ishanjoshi02@gmail.com>
2018-09-09 05:13:34 +0530

    Added code to save User Credentials in Cookies

--- src/user/ui/loginbutton/LoginButtonActions.js
+++ src/user/ui/loginbutton/LoginButtonActions.js
@@ -1,31 +1,46 @@
-import { uport } from './../../../util/connectors.js'
-import { browserHistory } from 'react-router'
+import { uport } from "./../../../util/connectors.js";
+import { browserHistory } from "react-router";
+import Cookies from "js-cookie";
 
-export const USER_LOGGED_IN = 'USER_LOGGED_IN'
+export const USER_LOGGED_IN = "USER_LOGGED_IN";
 function userLoggedIn(user) {
   return {
     type: USER_LOGGED_IN,
     payload: user
-  }
+  };
 }
 
 export function loginUser() {
   return function(dispatch) {
     // UPort and its web3 instance are defined in ./../../../util/wrappers.
     // Request uPort persona of account passed via QR
-    uport.requestCredentials().then((credentials) => {
-      dispatch(userLoggedIn(credentials))
+    uport
+      .requestCredentials({
+        requested: ["name", "avatar", "phone", "country"]
+      })
+      .then(credentials => {
+        dispatch(userLoggedIn(credentials));
+        console.log(credentials);
 
-      // Used a manual redirect here as opposed to a wrapper.
-      // This way, once logged in a user can still access the home page.
-      var currentLocation = browserHistory.getCurrentLocation()
+        // Used a manual redirect here as opposed to a wrapper.
+        // This way, once logged in a user can still access the home page.
 
-      if ('redirect' in currentLocation.query)
-      {
-        return browserHistory.push(decodeURIComponent(currentLocation.query.redirect))
-      }
+        // Save user credentials to cookies. Used library called js-cookie.
+        // Js-Cookie GitHub Repo : https://github.com/js-cookie/js-cookie
+        // Retrieve on browser window reload or restart so that user is still signed in
 
-      return browserHistory.push('/dashboard')
-    })
-  }
+        Cookies.set("userCredentials", credentials);
+        console.log(Cookies.getJSON("userCredentials"));
+
+        var currentLocation = browserHistory.getCurrentLocation();
+
+        if ("redirect" in currentLocation.query) {
+          return browserHistory.push(
+            decodeURIComponent(currentLocation.query.redirect)
+          );
+        }
+
+        return browserHistory.push("/dashboard");
+      });
+  };
 }
