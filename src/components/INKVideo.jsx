653179b7798c14c0bbbd192c4c3586f494f580f2
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2018-09-12 21:23:50 +0530

    Some minor changes.

--- src/components/INKVideo.jsx
+++ src/components/INKVideo.jsx
@@ -1,36 +1,13 @@
 import React, { Component } from "react";
 
 class INKVideo extends Component {
-  // state = {  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      // playing: false,
-      // buttonName: "Play",
-      source: this.props.src
-    };
-  }
-
-  // playPause = () => {
-  //   if (this.state.playing) {
-  //     this.setState({
-  //       playing: true,
-  //       buttonName: "Pause"
-  //     });
-  //   } else {
-  //     this.setState({
-  //       playing: false,
-  //       buttonName: "Play"
-  //     });
-  //   }
-  // };
+  state = {};
 
   render() {
     return (
       <div>
-        <video controls="true" width="1000" height="600">
-          <source src={this.state.source} />
+        <video controls="true" width="80%" height="50%">
+          <source src={this.props.src} />
         </video>
         {/* <button /> */}
       </div>
--- src/util/template.js
+++ src/util/template.js
@@ -2,7 +2,7 @@ import { Connect, SimpleSigner } from "uport-connect";
 import { SigningKey } from "../keys/uportkeys.js";
 
 export let uport = new Connect("INK", {
-  clientId: "2ozFbmN51xFHTdd97gCLRkMDNqCwey67xMA",
+  clientId: "your key",
   network: "rinkeby",
   signer: SimpleSigner(SigningKey)
 });
