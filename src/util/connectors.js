283be81b88bb023fb5ac261a84e147b37498215d
Ishan Joshi <ishanjoshi02@gmail.com>
2018-09-12 20:05:56 +0530

    added connectors.js

--- src/util/connectors.js
+++ src/util/connectors.js
@@ -2,7 +2,7 @@ import { Connect, SimpleSigner } from "uport-connect";
 import { SigningKey } from "../keys/uportkeys.js";
 
 export let uport = new Connect("INK", {
-  clientId: "2ozFbmN51xFHTdd97gCLRkMDNqCwey67xMA",
+  clientId: "2ouosXPNBs1vL8QhpMoASBE3xvG7qNicJPg",
   network: "rinkeby",
   signer: SimpleSigner(SigningKey)
 });
