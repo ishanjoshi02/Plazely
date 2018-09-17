997f95cd730bbb5c6f1f2d955c20fe51a64f9848
Ishan Joshi <ishanjoshi02@gmail.com>
2018-09-17 19:47:32 +0530

    removed commented out code

--- src/user/layouts/profile/Profile.js
+++ src/user/layouts/profile/Profile.js
@@ -46,38 +46,6 @@ class Profile extends Component {
         </div>
       </main>
     );
-
-    // return (
-    //   <main className="container">
-    //     <div className="pure-g">
-    //       <Card className="pure-u-1-1">
-    //         <h1>Profile</h1>
-    //         {/* <img src={AvatarURI} alt={this.props.authData.name} /> */}
-    //         <Image src={avatarURI} size="small" wrapped />
-    //         <p>
-    //           <strong>Name</strong>
-    //           <br />
-    //           {this.props.authData.name}
-    //         </p>
-    //         <p>
-    //           <strong>Email</strong>
-    //           <br />
-    //           {this.props.authData.email}
-    //         </p>
-    //         <p>
-    //           <strong>Phone</strong>
-    //           <br />
-    //           {this.props.authData.phone}
-    //         </p>
-    //         <p>
-    //           <strong>Country</strong>
-    //           <br />
-    //           {this.props.authData.country}
-    //         </p>
-    //       </Card>
-    //     </div>
-    //   </main>
-    // );
   }
 }
 
