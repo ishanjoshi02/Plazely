ab7d47e583cdb61a060ce11e9dae334182c84b7b
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2019-02-19 01:15:42 +0530

    Profile maybe

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Profile from "../Profile/index";
 
-const Home = () => {
-  return <React.Fragment>Home</React.Fragment>;
-};
+class Home extends React.Component {
+  state = {
+    name: "Neeraj"
+  };
+
+  render() {
+    return (
+      <div>
+        <Profile name={this.state.name} />
+      </div>
+    );
+  }
+}
 
 export default Home;
---
+++ client/src/components/Profile/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Profile = props => {
+  console.log(props);
+  return <div>Profile</div>;
+};
+
+export default Profile;
