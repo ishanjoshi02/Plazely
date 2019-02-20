6f64730168f497e41c4f4208a826e91b8254293e
Ishan Joshi <ishanjoshi02@gmail.com>
2019-02-20 08:47:26 +0530

    merged

--- truffle-config.js
+++ truffle-config.js
@@ -8,11 +8,7 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-<<<<<<< HEAD
-      network_id: "*" // Match any network id
-=======
       network_id: "*"
->>>>>>> dc99fc311fa99abf38c957928cebfe6a0cb63d62
     }
   }
 };
