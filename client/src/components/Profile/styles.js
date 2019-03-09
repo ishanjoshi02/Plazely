d053d1477868b640f6552336c12d8a50e045dc42
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2019-03-09 21:07:56 +0530

    Minor changes

---
+++ client/src/components/Profile/styles.js
@@ -0,0 +1,20 @@
+const styles = theme => ({
+  card: {
+    maxWidth: 350,
+    margin: "32px auto 0px auto"
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 24,
+    textAlign: "center"
+  },
+  pos: {
+    margin: "24px 12px"
+  }
+});
+
+export default styles;
